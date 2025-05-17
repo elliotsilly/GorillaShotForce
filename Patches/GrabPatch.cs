4b04f618abe4fce8af649c5f249383d0d5747ef9
elliot! <benjikrolfanpageuwu@gmail.com>
2025-05-17 10:08:43 +0100

    Add files via upload

---
+++ Patches/GrabPatch.cs
@@ -0,0 +1,24 @@
+﻿using GorillaLocomotion;
+using HarmonyLib;
+using UnityEngine;
+
+namespace GorillaShotForce.Patches
+{
+    [HarmonyPatch(typeof(Slingshot), "LateUpdateLocal")]
+    public class GrabPatch
+    {
+        public static void Prefix(Slingshot __instance)
+        {
+            if (__instance.itemState == TransferrableObject.ItemStates.State2
+             || __instance.itemState == TransferrableObject.ItemStates.State3)
+            {
+                Rigidbody rb = GTPlayer.Instance.bodyCollider.attachedRigidbody;
+                Vector3 velocity = rb.velocity * 0.995f;
+
+                
+                rb.velocity = velocity;
+                rb.AddForce(Physics.gravity * -0.4f * rb.mass * GTPlayer.Instance.scale);
+            }
+        }
+    }
+}
---
+++ Patches/LaunchPatch.cs
@@ -0,0 +1,23 @@
+﻿using GorillaShotForce.Tools;
+using HarmonyLib;
+using UnityEngine;
+
+namespace GorillaShotForce.Patches
+{
+    [HarmonyPatch(typeof(SlingshotProjectile), nameof(SlingshotProjectile.Launch))]
+    public class LaunchPatch
+    {
+        private static ShotForceEvents _shotForceEvents;
+
+        public static void Prefix(Vector3 position, Vector3 velocity, NetPlayer player)
+        {
+            bool isLocal = player.IsLocal;
+            bool inClump = !isLocal && Vector3.Distance(GorillaLocomotion.GTPlayer.Instance.bodyCollider.transform.position, position) <= 1f;
+            if (isLocal || inClump)
+            {
+                _shotForceEvents ??= new ShotForceEvents();
+                _shotForceEvents.Launch(velocity, inClump ? ForceType.Clump : ForceType.Self);
+            }
+        }
+    }
+}
